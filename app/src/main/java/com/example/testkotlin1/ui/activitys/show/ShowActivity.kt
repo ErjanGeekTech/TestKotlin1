5caeeed9c58038e41751f2362aa9e8aa448a3ce3
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-09-08 14:11:37 +0300

    add button when you press goes over MainActivity

--- app/src/main/java/com/example/testkotlin1/ui/activitys/show/ShowActivity.kt
+++ app/src/main/java/com/example/testkotlin1/ui/activitys/show/ShowActivity.kt
@@ -1,11 +1,14 @@
 package com.example.testkotlin1.ui.activitys.show
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.testkotlin1.R
+import com.example.testkotlin1.constants.Constants.EXTRA_INPUT_TEXT
 import com.example.testkotlin1.databinding.ActivityShowBinding
-import com.example.testkotlin1.ui.activitys.constants.Constants.EXTRA_INPUT_TEXT
+import com.example.testkotlin1.ui.activitys.main.MainActivity
 
 class ShowActivity : AppCompatActivity(R.layout.activity_show) {
 
@@ -14,9 +17,25 @@ class ShowActivity : AppCompatActivity(R.layout.activity_show) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialize()
+        setupListener()
     }
 
     private fun initialize() {
         binding.showText.text = intent.getStringExtra(EXTRA_INPUT_TEXT)
+    }
+
+    private fun setupListener() {
+        setupClickMainActivity()
+    }
+
+    private fun setupClickMainActivity() {
+        binding.nextActivityBtn.setOnClickListener {
+            if (!TextUtils.isEmpty(binding.showText.text.toString())) {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    putExtra(EXTRA_INPUT_TEXT, binding.showText.text.toString())
+                }
+                startActivity(intent)
+            }
+        }
     }
 }
